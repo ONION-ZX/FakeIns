19160c2d116a760661cb1450118efff3c927e113
ONION-ZX <zxonion@gmail.com>
2018-08-14 15:28:57 +0800

    initialize

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
+import 'iview/dist/styles/iview.css';
+import '../node_modules/normalize.css/normalize.css';
 
-Vue.config.productionTip = false
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import iView from 'iview';
+import Home from './page/Home';
+
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
+Vue.use(iView);
+
+const router = new VueRouter({
+  routes: [
+    {path: '/', component: Home },
+  ]
+})
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router,
 }).$mount('#app')
---
+++ src/page/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <Nav/>
+        <span>Yo.</span>
+    </div>
+</template>
+<script>
+import Nav from '../components/Nav.vue'
+export default {
+    components: { Nav },
+}
+</script>
+
+<style>
+
+</style>
+
